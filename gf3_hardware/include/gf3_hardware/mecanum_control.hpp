3f7d0ca58b0b4af6e7747fb84aa875a84995df00
JinSeok Kim/김진석 <jinseok.kim970@gmail.com>
2022-11-22 16:44:35 +0900

    add nav

--- gf3_hardware/include/gf3_hardware/mecanum_control.hpp
+++ gf3_hardware/include/gf3_hardware/mecanum_control.hpp
@@ -2,9 +2,14 @@
 #include <boost/asio.hpp>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/executor.hpp"
+#include "rclcpp/clock.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 #include <memory>
 #include <cstdio>
 #include <string.h>
@@ -29,13 +34,15 @@ class MecanumControl : public rclcpp::Node
     void state_callback(const can_msgs::msg::Frame & msg);
     int MainLoop();
     void test();
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    
 
   protected:
     
     rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr can_state_subscription_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
     rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr can_command_publisher_;
-    rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr odom_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
     // rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr can_command_publisher_;
 
     uint8_t can_data_[CAN_FRAME_MAX_LEN] = { 0xcf, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x07 };
@@ -43,6 +50,11 @@ class MecanumControl : public rclcpp::Node
     std::map<uint8_t, std::array<uint8_t, 8UL>> state_;
     void cmd_vel_callback(const geometry_msgs::msg::Twist &msg);
     void can_reply_callback(const can_msgs::msg::Frame &msg);
-    
+    double received_FL_w_, received_FR_w_, received_RL_w_,received_RR_w_;
+    bool is_FL_received_, is_FR_received_, is_RL_received_, is_RR_received_;
+    nav_msgs::msg::Odometry odom_;
+    can_msgs::msg::Frame m1_command_, m2_command_;
+    double x_, y_, th_;
+    double prev_time_;
 };
 }
--- gf3_hardware/src/mecanum_control.cpp
+++ gf3_hardware/src/mecanum_control.cpp
@@ -6,17 +6,31 @@ namespace gf3_hardware
   const double WHEEL_SEPARATION_LENGTH = 0.304;
   const double ratio = 10.0;
   const uint32_t motor_id_[2] = {0x001U, 0x002U}; // (FL - 0x002, M1), (RL - 0x002, M2), (FR - 0x001, M1), (RR - 0x001, M2)
+  const uint32_t motor_reply_id_[2] = {0x701U, 0x702U}; // (FL - 0x002, M1), (RL - 0x002, M2), (FR - 0x001, M1), (RR - 0x001, M2)
 
 MecanumControl::MecanumControl()
-:Node("MecanumControl")
+:Node("MecanumControl"),
+received_FL_w_(0),
+received_FR_w_(0),
+received_RL_w_(0),
+received_RR_w_(0),
+is_FL_received_(false),
+is_FR_received_(false),
+is_RL_received_(false),
+is_RR_received_(false),
+x_(0),
+y_(0),
+th_(0),
+prev_time_(rclcpp::Clock(RCL_ROS_TIME).now().seconds())
 {
   can_state_subscription_ = this->create_subscription<can_msgs::msg::Frame>(
     "can_feedback", 1, std::bind(&MecanumControl::can_reply_callback, this, _1));
   cmd_vel_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
     "cmd_vel", 1, std::bind(&MecanumControl::cmd_vel_callback, this, _1));
   can_command_publisher_ = this->create_publisher<can_msgs::msg::Frame>("can_command", 10);
-  odom_publisher_ = this->create_publisher<can_msgs::msg::Frame>("odom", 10);
-  
+  odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+  tf_broadcaster_ =
+      std::make_unique<tf2_ros::TransformBroadcaster>(*this);
   
 }
 
@@ -41,75 +55,143 @@ void MecanumControl::cmd_vel_callback(const geometry_msgs::msg::Twist &msg){
   // int16_t target_FR_rpm = 10;
   // int16_t target_RL_rpm = 10;
   // int16_t target_RR_rpm = 10;
+  m1_command_.data[0] = 0xCF;
+  m1_command_.data[1] = 0x01; // M1 enable
+  m1_command_.data[4] = 0x01; // M2 enable
+  m1_command_.data[7] = 0x07; // set reply type
+  
+  m2_command_.data[0] = 0xCF;
+  m2_command_.data[1] = 0x01; // M1 enable
+  m2_command_.data[4] = 0x01; // M2 enable
+  m2_command_.data[7] = 0x07; // set reply type
 
-  can_msgs::msg::Frame command;
-  command.data[0] = 0xCF;
-  command.data[1] = 0x01; // M1 enable
-  command.data[4] = 0x01; // M2 enable
-  command.data[7] = 0x07; // set reply type
+    // FL, RL
+  m1_command_.id = motor_id_[1];
+  m1_command_.data[2] = target_FL_rpm & 0xFF; // for FL(M1)
+  m1_command_.data[3] = (target_FL_rpm >> 8) & 0xFF;
+  m1_command_.data[5] = target_RL_rpm & 0xFF; // for RL(M2)
+  m1_command_.data[6] = (target_RL_rpm >> 8) & 0xFF;
 
-  for (uint8_t idx = 0 ;idx < 2; idx++){
-      
-      if (idx == 1){ // FL, RL
-        command.id = motor_id_[idx];
-        command.data[2] = target_FL_rpm & 0xFF; // for FL(M1)
-        command.data[3] = (target_FL_rpm >> 8) & 0xFF;
-        command.data[5] = target_RL_rpm & 0xFF; // for RL(M2)
-        command.data[6] = (target_RL_rpm >> 8) & 0xFF;
-      }
-      if (idx == 0){ // FR, RR
-        command.id = motor_id_[idx];
-        command.data[2] = target_FR_rpm & 0xFF; // for FR(M1)
-        command.data[3] = (target_FR_rpm >> 8) & 0xFF;
-        command.data[5] = target_RR_rpm & 0xFF; // for RR(M2)
-        command.data[6] = (target_RR_rpm >> 8) & 0xFF;
-      }
-      can_command_publisher_->publish(command);
-  }
+  m2_command_.id = motor_id_[0];
+  m2_command_.data[2] = target_FR_rpm & 0xFF; // for FR(M1)
+  m2_command_.data[3] = (target_FR_rpm >> 8) & 0xFF;
+  m2_command_.data[5] = target_RR_rpm & 0xFF; // for RR(M2)
+  m2_command_.data[6] = (target_RR_rpm >> 8) & 0xFF;
+  
 }
 
 void MecanumControl::can_reply_callback(const can_msgs::msg::Frame &msg)
 {
-  auto it = std::find(std::begin(motor_id_), std::end(motor_id_), msg.id);
-  if (it != std::end(motor_id_)) {
-      int index = std::distance(motor_id_, it);
+  auto it = std::find(std::begin(motor_reply_id_), std::end(motor_reply_id_), msg.id);
+  if (it != std::end(motor_reply_id_)) {
+      int index = std::distance(motor_reply_id_, it);
       std::cout << "Reply from Index: " << index << "found" << std::endl;
   } else {
       RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "not found...");
 
     }
   // c4(196)-motor1, c9(201)-motor2 
-  if (msg.id == 0x001U){ // Right controller
-    std::cout <<"ID 1 received" <<std::endl;
-    if (msg.data[0] == 0xC4U){ // FR
-
+  if (msg.id == 0x701U){ // Right controller/ (2 * M_PI) * 60 * ratio
+    if (msg.data[0] == 0xC4){ // FR
+      int16_t raw_rpm = ((msg.data[2] & 0xFF) +
+                        ((msg.data[3] << 8) & 0xFF'00));
+      received_FR_w_ = raw_rpm / 60 * (2* M_PI)  / ratio;
+      is_FR_received_ = true;
+      // RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "ID 1 - FR received: %f",received_FR_w_);
     }
-    if (msg.data[0] == 0xC9U){ // RR
-
-    }
-  }
-  else if(msg.id == 0x002U){ // Left controller
-    std::cout <<"ID 2 received" <<std::endl;
-    if (msg.data[0] == 0xC4U){ // FL
-
-    }
-    if (msg.data[0] == 0xC9U){ // RL
-
+    if (msg.data[0] == 0xC9){ // RR
+      int16_t raw_rpm = ((msg.data[2] & 0xFF) +
+                        ((msg.data[3] << 8) & 0xFF'00));
+      received_RR_w_ = raw_rpm / 60 * (2* M_PI)  / ratio;
+      is_RR_received_ = true;
+      // RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "ID 1 - RR received: %f",received_RR_w_);
     }
   }
-  int16_t rpm = msg.data[2] + ((msg.data[3] << 8) & 0xFF'00);
- 
-  int16_t received_FL_rpm_ = wheel_front_left / (2 * M_PI) * 60 * ratio;
-  int16_t received_FR_rpm_ = wheel_front_right / (2 * M_PI) * 60 * ratio;
-  int16_t received_RL_rpm_ = wheel_rear_left / (2 * M_PI) * 60 * ratio;
-  int16_t received_RR_rpm_ = wheel_rear_right / (2 * M_PI) * 60 * ratio;
-  
+  else if(msg.id == 0x702U){ // Left controller
+    if (msg.data[0] == 0xC4){ // FL
+      int16_t raw_rpm = ((msg.data[2] & 0xFF) +
+                        ((msg.data[3] << 8) & 0xFF'00));
+      received_FL_w_ = raw_rpm / 60 * (2* M_PI)  / ratio;
+      is_FL_received_ = true;
+      // RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "ID 1 - FL received: %f",received_FR_w_);
+    }
+    if (msg.data[0] == 0xC9){ // RL
+      int16_t raw_rpm = ((msg.data[2] & 0xFF) +
+                        ((msg.data[3] << 8) & 0xFF'00));
+      received_RL_w_ = raw_rpm / 60 * (2* M_PI)  / ratio;
+      is_RL_received_ = true;
+      // RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "ID 1 - RL received: %f",received_FR_w_);
+    }
+  }
 }
 
 
 int MecanumControl::MainLoop()
 {
+  can_command_publisher_->publish(m1_command_);
+  can_command_publisher_->publish(m2_command_);
   rclcpp::spin_some(shared_from_this());
+  if (is_FL_received_ == true && is_FR_received_ == true &&is_RL_received_ == true &&is_RR_received_ == true){ // if All w are received
+    double vx = (received_FR_w_ + received_FL_w_ + received_RL_w_ + received_RR_w_) * WHEEL_RADIUS / 4 ;
+    double vy = (received_FR_w_ - received_FL_w_ + received_RL_w_ - received_RR_w_) * WHEEL_RADIUS / 4 ;
+    double vth = (received_FR_w_ - received_FL_w_ - received_RL_w_ + received_RR_w_) * WHEEL_RADIUS / (4 * (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH));
+    
+    double dt = rclcpp::Clock(RCL_ROS_TIME).now().seconds() - prev_time_;
+    prev_time_ = rclcpp::Clock(RCL_ROS_TIME).now().seconds();
+    // RCLCPP_INFO(rclcpp::get_logger("MecanumControl"), "dt calculated: %f",dt);
+    double delta_x = ((vx * cos(th_) - vy * sin(th_)) * dt);
+    double delta_y = ((vx * sin(th_) + vy * cos(th_)) * dt);
+    double delta_th = vth * dt;
+
+    x_ += delta_x;
+    y_ += delta_y;
+    th_ += delta_th;
+
+   
+    odom_.header.stamp = rclcpp::Clock(RCL_ROS_TIME).now();
+    odom_.child_frame_id = "base_link";
+    odom_.twist.twist.linear.x = vx;
+    odom_.twist.twist.linear.y = vy;
+    odom_.twist.twist.angular.z = vth;
+
+    odom_.pose.pose.position.x = x_;
+    odom_.pose.pose.position.y = y_;
+    // odom_.pose.pose.orientation += th_;
+
+    odom_publisher_->publish(odom_);
+   
+
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = rclcpp::Clock(RCL_ROS_TIME).now();
+    t.header.frame_id = "odom";
+    t.child_frame_id = "base_link";
+
+    // Turtle only exists in 2D, thus we get x and y translation
+    // coordinates from the message and set the z coordinate to 0
+    t.transform.translation.x = x_;
+    t.transform.translation.y = y_;
+    t.transform.translation.z = 0.0;
+
+    // For the same reason, turtle can only rotate around one axis
+    // and this why we set rotation in x and y to 0 and obtain
+    // rotation in z axis from the message
+    tf2::Quaternion q;
+    q.setRPY(0, 0, th_);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+
+
+    is_FR_received_ = false;
+    is_FL_received_ = false;
+    is_RL_received_ = false;
+    is_RR_received_ = false;
+  }
   return 0;
 }
 }
@@ -117,9 +199,11 @@ int MecanumControl::MainLoop()
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
+  rclcpp::Rate loop_rate(100);
   auto mecanum = std::make_shared<gf3_hardware::MecanumControl>();
   while(rclcpp::ok()){
     mecanum->MainLoop();
+    // loop_rate.sleep();
   }
   rclcpp::shutdown();
   return 0;
