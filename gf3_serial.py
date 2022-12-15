6ecdd6020d18a361677b2dc85759442581c13c37
Jinseok Kim <jinseok.kim970@gmail.com>
2022-12-15 11:18:02 +0900

    fix serial

--- gf3_serial.py
+++ gf3_serial.py
@@ -16,6 +16,41 @@ while True:
     elif msg == '2':
         cw = [0xff, 0x02]
         ser.write(serial.to_bytes(cw))
+    elif msg == '3':
+        cw = [0xff, 0x03]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '4':
+        cw = [0xff, 0x04]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '5':
+        cw = [0xff, 0x05]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '6':
+        cw = [0xff, 0x06]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '7':
+        cw = [0xff, 0x07]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '8':
+        cw = [0xff, 0x08]
+        ser.write(serial.to_bytes(cw))
+    elif msg == '9':
+        cw = [0xff, 0x09]
+        ser.write(serial.to_bytes(cw))
+    elif msg == 'a':
+        cw = [0xff, 0x0a]
+        ser.write(serial.to_bytes(cw))
+    elif msg == 'b':
+        cw = [0xff, 0x0b]
+        ser.write(serial.to_bytes(cw))
+    elif msg == 'c':
+        cw = [0xff, 0x0c]
+        ser.write(serial.to_bytes(cw))
+    elif msg == 'd':
+        cw = [0xff, 0x0d]
+        ser.write(serial.to_bytes(cw))
+
+
         
 ser.close()
 
